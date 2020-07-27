4aaa3c86c223bb6573e60a9c428804d3a71cadf8
Gary Black <gar2000b@yahoo.com>
2020-07-27 15:49:05 -0400

    setting id for replaceone as string

--- src/main/java/com/onlineinteract/workflow/domain/account/repository/AccountRepository.java
+++ src/main/java/com/onlineinteract/workflow/domain/account/repository/AccountRepository.java
@@ -40,7 +40,7 @@ public class AccountRepository {
 		MongoDatabase database = dbClient.getMongoClient().getDatabase(DbClient.DATABASE);
 		Document accountDocument = Document.parse(account.toString());
 		MongoCollection<Document> accountsCollection = database.getCollection("accounts");
-		accountsCollection.replaceOne(new Document("id", account.getId()), accountDocument);
+		accountsCollection.replaceOne(new Document("id", account.getId().toString()), accountDocument);
 		System.out.println("Account Updated in accounts collection");
 	}
 
@@ -71,7 +71,7 @@ public class AccountRepository {
 		MongoDatabase database = dbClient.getMongoClient().getDatabase(DbClient.DATABASE);
 		Document accountDocument = Document.parse(account.toString());
 		MongoCollection<Document> accountsCollection = database.getCollection("accounts");
-		accountsCollection.replaceOne(new Document("id", account.getId()), accountDocument);
+		accountsCollection.replaceOne(new Document("id", account.getId().toString()), accountDocument);
 		System.out.println("Account Updated in accounts collection");
 	}
 	
